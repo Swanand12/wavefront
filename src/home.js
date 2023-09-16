dd35b466d9803fd0f3004f728bdee9150bc8d6b7
Swanand12 <sawantswanand2004@gmail.com>
2023-09-16 15:55:45 +0530

    First Commit

---
+++ src/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./navbar";
+
+
+function Home() {
+    return(
+        <>
+        <Navbar/>
+        <h1>Sawant</h1>
+        </>
+    )
+}
+
+export default Home;
